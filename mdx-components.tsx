f262b3ccdfc4c895df74e42e2b6884463a394115
onebignick <nicholas.zhengkai.ong@gmail.com>
2025-06-21 19:20:25 +0800

    feat: raii

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -5,6 +5,10 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     return { 
         h1: (props) => <h1 {...props} className="mb-4 text-4xl font-bold" />,
         h2: (props) => <h2 {...props} className="invisible"/>,
+        h3: (props) => <h2 {...props} className="mb-4 text-2xl font-bold" />,
+        h4: (props) => <h3 {...props} className="mb-4 text-l font-bold" />,
+        ol: (props) => <ol {...props} className="list-decimal"/>,
+        li: (props) => <li {...props}/>,
         p: (props) => <p {...props} className="mb-4" />,
         pre: (props) => (
             <pre {...props} className="rounded-lg border-2 border-zinc-500 p-4 mb-4 overflow-y-scroll" />    
